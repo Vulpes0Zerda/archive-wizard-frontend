efd9725e224c2f0566873b8ca26e61f2c9a07f12
Lucy B. <lucyburock@gmail.com>
2026-09-17 14:50:17 +0200

    added shelf model and fixed the switchup between shelf response and request

--- src/app/services/model/Shelf.ts
+++ src/app/services/model/Shelf.ts
@@ -2,28 +2,30 @@ import { CategoryGroup } from './CategoryGroup';
 
 export namespace Shelf {
   export namespace Response {
-    export type GetAll = [
-      {
-        id: number;
-        categoryGroupId: number;
-        name: string;
-        position: number;
-      },
-    ];
-    export type PostSingle = [
-      {
-        id: number;
-        categoryGroupId: number;
-        name: string;
-        position: number;
-      },
-    ];
+    export type GetAll = Array<{
+      id: number;
+      categoryGroup: CategoryGroup.Model;
+      name: string;
+      position: number;
+    }>;
+    export type PostSingle = Array<{
+      id: number;
+      categoryGroup: CategoryGroup.Model;
+      name: string;
+      position: number;
+    }>;
   }
   export namespace Request {
     export type PostSingle = {
-      categoryGroup: CategoryGroup.Model;
+      categoryGroupId: number;
       name: string;
       position: number;
     };
   }
+  export type Model = {
+    id: number;
+    categoryGroup: CategoryGroup.Model;
+    name: string;
+    position: number;
+  };
 }
