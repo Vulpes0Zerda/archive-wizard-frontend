1613947a371477874a0dffc7db7035ff740bfc8e
Lucy B. <lucyburock@gmail.com>
2026-09-19 02:21:33 +0200

    changed api responses to return a HttpResponse typed with the response data type  instead of returning the type itself, to allow for better response management and error handeling

--- src/app/services/api/shelf.api.ts
+++ src/app/services/api/shelf.api.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Shelf } from '../model/Shelf';
@@ -10,14 +10,16 @@ import { ApiService } from './api.service';
 export class ShelfApi {
   constructor(private http: HttpClient) {}
 
-  public getShelfs(): Observable<Shelf.Response.GetAll> {
-    return this.http.get<Shelf.Response.GetAll>(
+  public getShelfs(): Observable<HttpResponse<Shelf.Response.GetAll>> {
+    return this.http.get<HttpResponse<Shelf.Response.GetAll>>(
       `${ApiService.BASE_URL}/shelf-manager/get-overview/`,
     );
   }
 
-  public postShelf(newShelf: Shelf.Request.PostSingle): Observable<Shelf.Response.PostSingle> {
-    return this.http.post<Shelf.Response.PostSingle>(
+  public postShelf(
+    newShelf: Shelf.Request.PostSingle,
+  ): Observable<HttpResponse<Shelf.Response.PostSingle>> {
+    return this.http.post<HttpResponse<Shelf.Response.PostSingle>>(
       `${ApiService.BASE_URL}/shelf-manager/create-shelf`,
       newShelf,
     );
