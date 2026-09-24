1c529b2ebbb45140e0829f223971b5bf9d5ac02a
Liliana Domke <liliana.domke@student.htw-berlin.de>
2026-09-24 19:26:07 +0200

    added shelf component

---
+++ src/app/shelf/shelf.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-shelf',
+  styleUrl: './shelf.scss',
+  templateUrl: './shelf.html',
+})
+export class Shelf {}
